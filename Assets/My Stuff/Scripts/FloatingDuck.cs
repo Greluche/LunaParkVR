0b938020680831ba9427495a14147063c6769f40
arielpelayo <146746625+arielpelayo@users.noreply.github.com>
2025-05-10 16:23:26 +0200

    duck trajectory + beginning of jail

--- Assets/My Stuff/Scripts/FloatingDuck.cs
+++ Assets/My Stuff/Scripts/FloatingDuck.cs
@@ -4,9 +4,8 @@ public class FloatingDuck : MonoBehaviour
 {
     public float angularSpeed = .3f;
     public float radius = 2f;
-    public float test = 1f;
-    
-    private float angle = 0f;
+    public float shift = 0f;
+    public Transform center;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,17 +16,13 @@ public class FloatingDuck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Mathf.Cos(Time.time * angularSpeed) * radius;
-        float y = transform.position.y;
-        float z = Mathf.Sin(Time.time * angularSpeed) * radius;
+        // trajectory
+        float x = Mathf.Cos((Time.time + shift) * angularSpeed) * radius;
+        float z = Mathf.Sin((Time.time + shift) * angularSpeed) * radius;
+        // illusion of floating
+        float y = Mathf.PingPong(Time.time + shift, .05f);
+        
         transform.position = new Vector3(x, y, z);
-        
-        angle += Time.deltaTime * test * angularSpeed;
-        transform.Rotate(0, - Time.deltaTime * angularSpeed * test, 0, Space.Self);
-        
-        //vertical = Input.GetAxis("Vertical");
-        //horizontal = Input.GetAxis("Horizontal");
-        //Vector3 direction = Quaternion.Euler(0, angle, 0);
-        //transform.position = center + direction * radius;
+        transform.LookAt(center);
     }
 }
